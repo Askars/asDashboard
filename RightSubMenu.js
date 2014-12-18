56f2645e704a5489554920707cc532c8f76783a0
Askars <not_a_real_email@email.com>
2014-12-19 08:30:19 +1000

    Right menu development

--- RightSubMenu.js
+++ RightSubMenu.js
@@ -4,17 +4,35 @@ RightSubMenuBtnObj = function(parent, titleTextArr, text, description, onclick, 
     self.parentMenu = parent;
     self.leftTitleText = titleTextArr[0];
     self.rightTitleText = titleTextArr[1];
-    self.subMenu = new RightSubMenuObj(270);
+    
 }
 
 
 RightSubMenuObj = function(width) {
     var self = this;
     self.parent  = null;
-    self.contentDiv = null;
+    self.wrapperDiv = null;
     self.width = width;
     self.buttons = [];
     self.expanded = false;
+    
+    // We do this one a little differently, we own the contentDiv and the parent supplies a wrappingDiv
+    // This is because we want to keep everything in the menu self contained for detach/reattachments.
+    self.contentDiv = document.createElement('div');
+    
+    var titleDiv = document.createElement('div');
+    $(titleDiv).addClass('RightMenuSubTitle').appendTo($(self.contentDiv))
+    
+    self.leftSubTitleDiv = document.createElement('div');
+    self.rightSubTitleDiv = document.createElement('div');
+    $(self.leftSubTitleDiv).addClass("LeftTextDiv").appendTo($(titleDiv));
+    $(self.rightSubTitleDiv).addClass("RightTextDiv").appendTo($(titleDiv));
+    
+    var floatClear = document.createElement('div');
+    $(floatClear).css('clear', 'both').appendTo($(titleDiv));
+    
+    titleDiv.appendChild(floatClear);
+
 };
 
 RightSubMenuObj.prototype.setSubTitle = function (leftText, rightText) {
